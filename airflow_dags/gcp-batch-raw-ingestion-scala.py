ec622e5a0174317f88e9aae89ef4baca19e493dc
amakadam1994 <amar.kadam@clairvoyantsoft.com>
2023-12-21 14:55:15 +0530

    Dag updated

--- airflow_dags/gcp-batch-raw-ingestion-scala.py
+++ airflow_dags/gcp-batch-raw-ingestion-scala.py
@@ -12,7 +12,7 @@ SPARK_JOB = {
     "reference": {"project_id": "playground-375318"},
     "placement": {"cluster_name": "gcp-batch-raw-ingestion-scala"},
     "spark_job": {
-        "jar_file_uris": ["gs://bronze-poc-group/gcp-batch-raw-ingestion-scala/cv-gcp-etl-jar-with-dependencies.jar","gs://bronze-poc-group/gcp-batch-raw-ingestion-scala/jars/scopt_2.12-4.0.1.jar","gs://bronze-poc-group/gcp-batch-raw-ingestion-scala/jars/spark-3.1-bigquery-0.31.1.jar"],
+        "jar_file_uris": ["gs://bronze-poc-group/gcp-batch-raw-ingestion-scala/gcp-batch-raw-ingestion-scala-jar-with-dependencies.jar","gs://bronze-poc-group/gcp-batch-raw-ingestion-scala/jars/scopt_2.12-4.0.1.jar","gs://bronze-poc-group/gcp-batch-raw-ingestion-scala/jars/spark-3.1-bigquery-0.31.1.jar"],
         "main_class": "com.clairvoyant.app.CLAIRVOYANTApp",
         "properties": {
         "spark.driver.extraJavaOptions":"-DdoneFolderName=poc_folder -DtemporaryGcsBucket=dataproc_ravi_poc -Dproject_id=playground-375318 -Dsrc_root=gs://bronze-poc-group/mongodb/landing -Denvironment=cluster -DsrcSchema=sample_analytics -DdistSchema=transactional_data -Dconfig.resource=/PROD/application.conf",
